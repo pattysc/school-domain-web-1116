91e471d73f4c46eabe1846f714da3d9376768cbd
pattysc <pattys@users.noreply.github.com>
2016-11-30 16:27:47 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+require 'pry'
+
+class School
+  def initialize(schoolname)
+    @schoolname = schoolname
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(studentname, grade)
+    if @roster[grade]
+      @roster[grade] << studentname
+    else
+      @roster[grade] = []
+      @roster[grade] << studentname
+    end
+  end
+
+  def grade(gradelevel)
+    @roster[gradelevel]
+  end
+
+  def sort
+    @roster.each do |gradelevel, arraystudents|
+      arraystudents.sort!
+    end
+  end
+end
+
+aschool = School.new("McGill")
+aschool.add_student("Ashley", 9)
